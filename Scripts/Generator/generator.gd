687005ce9427f2277ba8b097187b81c05bbe966e
Pedro Lealdini <pedrolealdini@gmail.com>
2024-05-22 17:03:31 -0300

    Cleaned and added typing to previous work.

--- Scripts/Generator/generator.gd
+++ Scripts/Generator/generator.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	var salpeter_generator = Salpeter.new()
-	salpeter_generator.salpeter_generation(0.08, 110, 5000000)
+	print(salpeter_generator.salpeter_generation(0.08, 110, 5))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- Scripts/Libraries/Salpeter_ly.gd
+++ Scripts/Libraries/Salpeter_ly.gd
@@ -1,25 +1,18 @@
-#@tool
+## Defines the Salpeter class that generates a list of stellar masses in solar masses accorsing to Salpeter's distribution
 class_name Salpeter
 
-#func _run():
-	#salpeter_generation(0.08, 110, 5000000)
-
-func salpeter_generation(minimum_mass, maximum_mass, number_of_stars):
-	var alpha = 2.35 # constant as defined by Salpeter in 1955
-	var exponent = -alpha + 1
-	var salpeter_rng = RandomNumberGenerator.new()
+##
+func salpeter_generation(minimum_mass, maximum_mass, number_of_stars) -> Array[float]:
+	var alpha: float = 2.35 # constant as defined by Salpeter in 1955
+	var exponent: float = -alpha + 1
+	var salpeter_rng: RandomNumberGenerator = RandomNumberGenerator.new()
 	
-	var x = [] # uniformly distributed random numbers between 0 and 1
+	var x: Array[float] # uniformly distributed random numbers between 0 and 1
 	
-	for i in range(number_of_stars+1):
+	for i: int in range(number_of_stars+1):
 		x.append(salpeter_rng.randf())
 	
-	#for i in range(num_stars):
-	#masses[i] = ((m_max**exponent - m_min**exponent) * x[i] + m_min**exponent)**(1.0/exponent)
-	
-	for i in range(number_of_stars+1):
-		x[i] = ((maximum_mass**exponent - minimum_mass**exponent) * x[i] + minimum_mass**exponent)**(1.0/exponent)
+	for mass: float in x:
+		mass = ((maximum_mass**exponent - minimum_mass**exponent) * mass + minimum_mass**exponent)**(1.0/exponent)
 		
-	for a in x:
-		if a > 20:
-			print(a)
+	return x
